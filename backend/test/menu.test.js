39cb22341263091c6f2d839c2c0e8abca97023fd
Charles Osegi <charlesosegi@gmail.com>
2019-02-23 18:54:03 +0100

    wrote test for posting a menu endpoint

--- backend/test/menu.test.js
+++ backend/test/menu.test.js
@@ -16,5 +16,26 @@ describe('Menu', () => {
       });
     });
   });
- 
+  describe('/POST menu', () => {
+    it('it should post a menu', (done) => {
+      let meal = {
+        id: 1,
+        name: 'White Rice',
+        size: 'plates',
+        price: '500',
+        currency: 'NGN'
+      }
+      chai.request(app).post('/api/v1/menu/add').send(meal).end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message').eql('Menu successfully added!');
+        res.body.data.should.have.property('id');
+        res.body.data.should.have.property('name');
+        res.body.data.should.have.property('size');
+        res.body.data.should.have.property('price');
+        res.body.data.should.have.property('currency');
+        done();
+      });
+    });
+  });  
 });
